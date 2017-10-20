5e4cb954a8174422485856584a7b01272318553d
Sam Hoffman <samdhoffman@gmail.com>
2017-10-20 11:58:32 -0400

    add post route to users

--- routes/users.js
+++ routes/users.js
@@ -20,35 +20,64 @@ router.get('/users', (req, res, next) => {
 })
 
 
+// router.post('/users', (req, res, next) => {
+//
+//   knex('users')
+//     .then(user_info => {
+//       const {first_name, last_name, email, hashed_password} = req.body;
+//       console.log(req.body);
+//       let newUser = {};
+//       if (first_name) {
+//         newUser.first_name = first_name;
+//       }
+//
+//       if (last_name) {
+//         newUser.last_name = last_name;
+//       }
+//
+//       if (email) {
+//         newUser.email = email;
+//       }
+//
+//       if (hashed_password) {
+//         newUser.hashed_password = hashed_password;
+//       }
+//
+//       return knex('users')
+//         .insert(newUser);
+//
+//     })
+//     bcrypt.hash(req.body.hashed_password, 12)
+//       .then(hashed_password => {
+//         console.log(hashed_password);
+//         res.sendStatus(200);
+//       })
+//       .catch(err => {
+//         next(err);
+//       })
+//     .then(data => {
+//       const user = data[0];
+//       res.send(user);
+//     })
+//     .catch(err => {
+//       next(err);
+//     })
+// })
+
 router.post('/users', (req, res, next) => {
-
-  knex('users')
-    .then(user_info => {
-      const {first_name, last_name, email, hashed_password} = req.body;
-      let newUser = {};
-      if (first_name) {
-        newUser.first_name = first_name;
-      }
-
-      if (last_name) {
-        newUser.last_name = last_name;
-      }
-
-      if (email) {
-        newUser.email = email;
-      }
-
-      if (hashed_password) {
-        newUser.hashed_password = bcrypt.hash(req.body.hashed_password, 12);
-      }
-
+  bcrypt.hash(req.body.hashed_password, 12)
+    .then(hashed_password => {
+      console.log(hashed_password);
       return knex('users')
-        .insert(newUser);
-
+        .insert({
+          first_name: req.body.first_name,
+          last_name: req.body.last_name,
+          email: req.body.email,
+          hashed_password: hashed_password
+        });
     })
     .then(data => {
-      const user = data[0];
-      res.send(user);
+      res.send(data);
     })
     .catch(err => {
       next(err);
